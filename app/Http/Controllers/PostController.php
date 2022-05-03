d732ef0241db6394f3b2f2dea79749fcc7b9c29d
Miko2495 <n037pybn125qio@i.softbank.jp>
2022-05-03 17:53:28 +0900

    8-2(ページネーションまで)終了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,6 @@ class PostController extends Controller
     public function index(Post $post)
 {
 
-    return view('index')->with(['posts' => $post->getByLimit()]);
+    return view('index')->with(['posts' => $post->getPaginateByLimit()]);
     }
 }

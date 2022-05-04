928796f428a7beca42ccaf5a11d8f0d633d961b3
Miko2495 <n037pybn125qio@i.softbank.jp>
2022-05-04 23:38:05 +0900

    ブログ投稿作成画面の作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,7 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='create'>[<a href='/posts/create'>create</a>]</p>
         <div class='posts'>
             @foreach($posts as $post)
             <div class='post'>
@@ -25,6 +26,6 @@
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
-　　　　</h2>
+
     </body>
 </html>
